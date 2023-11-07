87af97481229228ef0a4758d58a88fd5f6ed47d1
Centtent <144763708+Centtent@users.noreply.github.com>
2023-11-07 16:40:34 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+waktu masak mie 
+jika mie dimasak dan pas waktunya 5 menit mi akan matang
+jika mie dimasak diatas waktu 5 menit mie akan kematengan
+jika mie dimasak dibawah waktu 5 menit mie akan tidak matang
---
+++ script.js
@@ -0,0 +1,9 @@
+let waktumasak = prompt("masukan berapa lama waktunya")
+if(waktumasak > 5){
+    alert("Mie kematengan")
+}else if(waktumasak == 5){
+    alert("Mie sudah matang")
+}else{ alert("Mei tidak matang")}
+
+
+
